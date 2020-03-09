b5cb47242bf3e0e80709b9fc5bd2128183cd2e03
Miguel Molina Jr <mmolina@sscinc.com>
2020-03-09 10:46:54 -0400

    initial solution commit

---
+++ LoggingExamples/LoggingExamples.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LoggingExamples.Console
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
